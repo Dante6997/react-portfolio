1fb692d1fce106f45b923cad0c62f16ad8efb3ae
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-11-12 13:04:20 -0500

    fixing deployemnt

---
+++ README.md
@@ -0,0 +1,10 @@
+# REACT PORTFOLIO
+
+## Description
+* Being a web developer means being part of a community. You’ll need a place to share your projects not only if you're applying for jobs or working as a freelancer but also so that you can share your work with fellow developers and collaborate on future projects. Now that you’ve completed multiple projects, my task is to create a portfolio, using your new React skills to help set you apart from other developers whose portfolios don’t use the latest technologies.
+
+# Screenshot
+* ![image](https://user-images.githubusercontent.com/107078530/201269973-1a2ba432-5e19-46e5-8a3b-ff15b5b794b0.png)
+
+# Link to deployed application
+* https://dante6997.github.io/react-portfolio
---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import coverImage from "../../assets/large/0.jpeg";
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Dante's World</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Hello! My name is Dante Rodriguez and I am currently a coding bootcamp student. I graduated from Buffalo State College and I obtained my bachelors in Mechanical Engineering.
+          I love playing basketball and playing video games. As a young kid, I always had an interest in animals. My dream job was to become a Vet and help animals across the globe. Now, I 
+          have found a new interest in coding.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
