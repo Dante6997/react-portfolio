72b82b235173b0675cdd720cb4e2a00e9fca6835
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-11-17 20:37:42 -0500

    added project3

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,6 +12,10 @@ function About() {
           I love playing basketball and playing video games. As a young kid, I always had an interest in animals. My dream job was to become a Vet and help animals across the globe. Now, I 
           have found a new interest in coding.
         </p>
+        <div>
+          <h2>Project 3</h2>
+          <img src="https://youseph-codes.github.io/Anime-Watchify/" alt='project 3' />
+        </div>
       </div>
     </section>
   );
